1d64ea30736660b13c94df85fd5919f30d2d3794
Yogeshbairwa <ybairwa786@gmail.com>
2024-05-08 16:42:05 +0530

    updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -57,21 +57,23 @@ export const registerController = async (req, res) => {
 export const registerController = async (req, res, next) => {
   const { name, email, password } = req.body;
 
-  if (!name) {
-    next("name is required");
-  }
-  if (!email) {
-    next("Email is required");
-  }
-  if (!password) {
-    next("password is required");
-  }
-  const existingUser = await userModel.findOne({ email });
+  //   //   if (!name) {
+  //   //     next("name is required");
+  //   //   }
+  //   //   if (!email) {
+  //   //     next("Email is required");
+  //   //   }
+  //   //   if (!password) {
+  //   //     next("password is required");
+  //   //   }
+  //   const existingUser = await userModel.findOne({ email });
 
-  if (existingUser) {
-    next("Email already registered please login");
-  }
+  //   if (existingUser) {
+  //     next("Email already registered please login");
+  //   }
+
   const user = await userModel.create({ name, email, password });
+
   res.status(201).send({
     success: true,
     message: "user created successfully",
--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import validator from "validator";
+import bcrypt from "bcryptjs";
 
 //Schema
 const userSchema = new mongoose.Schema(
@@ -29,5 +30,10 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+//hashing password
+userSchema.pre("save", async function () {
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 export default mongoose.model("User", userSchema);
